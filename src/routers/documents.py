5ea5ed38ad6f7a1a64e2c9944ed252b728960fe6
Boris <108773158+NoisGit@users.noreply.github.com>
2026-05-10 07:05:49 -0400

    Remove subadmin from document permissions

--- src/routers/documents.py
+++ src/routers/documents.py
@@ -61,7 +61,6 @@ async def list_my_company_documents(
         RoleChecker(
             [
                 UserRole.ADMIN,
-                UserRole.SUBADMIN,
             ],
         ),
     ),
@@ -87,7 +86,6 @@ async def get_document_detail(
             [
                 UserRole.SUPERADMIN,
                 UserRole.ADMIN,
-                UserRole.SUBADMIN,
             ],
         ),
     ),
@@ -112,7 +110,6 @@ async def download_document(
             [
                 UserRole.SUPERADMIN,
                 UserRole.ADMIN,
-                UserRole.SUBADMIN,
             ],
         ),
     ),
